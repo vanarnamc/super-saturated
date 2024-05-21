adb55becb7f5e0f22774fa65484217bd2b98e527
clinton-van-arnam <113942027+vanarnamc@users.noreply.github.com>
2024-05-21 07:51:30 -0400

    update

--- about/about.js
+++ about/about.js
@@ -8,3 +8,39 @@ function checkThumbnailPosition(e) {
     }
 }
 window.addEventListener('mousemove', checkThumbnailPosition);
+
+// Get all the about-designer-number elements
+var designerNumbers = document.querySelectorAll('.about-designer-number');
+
+// Add a click event listener to each about-designer-number
+designerNumbers.forEach(function (designerNumber) {
+    designerNumber.addEventListener('click', function (e) {
+        // Prevent the window click event from firing
+        e.stopPropagation();
+
+        // Get the corresponding about-designer-thumbnail
+        var thumbnail = this.parentNode.parentNode.querySelector('.about-designer-thumbnail');
+
+        // Center the thumbnail
+        thumbnail.style.position = 'fixed';
+        thumbnail.style.top = '50%';
+        thumbnail.style.left = '50%';
+        thumbnail.style.transform = 'translate(-50%, -50%)';
+        thumbnail.style.zIndex = '1000';
+    });
+});
+
+// Add a click event listener to the window to hide the image
+window.addEventListener('click', function () {
+    // Get all the about-designer-thumbnail elements
+    var thumbnails = document.querySelectorAll('.about-designer-thumbnail');
+
+    // Reset the style of each thumbnail
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.style.position = '';
+        thumbnail.style.top = '';
+        thumbnail.style.left = '';
+        thumbnail.style.transform = '';
+        thumbnail.style.zIndex = '';
+    });
+});
